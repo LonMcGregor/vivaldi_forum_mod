14daa9b4e13df51db890c7440130382b26be6fe0
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2018-11-07 20:38:23 +0000

    observing more dom crap

--- mods/native-notifications.js
+++ mods/native-notifications.js
@@ -11,7 +11,7 @@ function checkPermission(){
 
 const OBSERVER = new MutationObserver(onMutate);
 const UNREAD_CONFIG = {attributes: true, attributeFilter: ["data-content"]};
-let NOTIF_LIST, CHAT_LIST;
+let NOTIF_LIST, CHAT_LIST, MOBILE_NOTIF_LIST, MOBILE_CHAT_LIST;
 
 /**
  * Handle the response from the background script indicating
@@ -39,13 +39,54 @@ function simulateOpenClose(button){
 }
 
 /**
+ * Get the list of notifications for a given unread badge
+ * @param {string} listId "chat_dropdown" or "notif_dropdown"
+ */
+function getList(listId){
+    const isMobile = window.innerWidth < 992;
+    if(listId!=="chat_dropdown" && listId!=="notif_dropdown"){
+        throw "Incorrect list id "+listId;
+    }
+    const isChat = listId==="chat_dropdown";
+    if(isMobile && isChat){
+        if(!MOBILE_CHAT_LIST){
+            simulateOpenClose(document.getElementById("mobile-chats"));
+            MOBILE_CHAT_LIST = document.querySelector("#chats-menu > section > ul");
+        }
+        return MOBILE_CHAT_LIST;
+    }else if(isMobile && !isChat){
+        if(!MOBILE_NOTIF_LIST){
+            simulateOpenClose(document.getElementById("mobile-menu"));
+            MOBILE_NOTIF_LIST = document.querySelector("#menu > section:nth-child(5) > ul");
+        }
+        return MOBILE_NOTIF_LIST;
+    }else if(!isMobile && isChat){
+        if(!CHAT_LIST){
+            simulateOpenClose(document.querySelector("#chat_dropdown"));
+            CHAT_LIST = document.querySelector(".chats .chat-list");
+        }
+        return CHAT_LIST;
+    }else if(!isMobile && !isChat){
+        if(!NOTIF_LIST){
+            simulateOpenClose(document.querySelector("#notif_dropdown"));
+            NOTIF_LIST = document.querySelector(".notifications .notification-list");
+        }
+        return NOTIF_LIST;
+    }
+}
+
+/**
  * One of the unread count badges has changed
  * @param {DOMElement} target that changed
  */
 function newUnreadData(target){
     console.log("New unread", target);
-    const parent = target.parentElement;
-    const list = parent.id==="chat_dropdown" ? CHAT_LIST : NOTIF_LIST;
+    const unreadId = target.parentElement.id;
+    const list = getList(unreadId);
+    if(!list){
+        console.error("Failed to get list", unreadId);
+        return;
+    }
     const latest = list.children[0];
     if(!latest.classList.contains("unread")){
         console.warn("Tried to read a notification that was already read / notifications haven't loaded yet", latest);
@@ -77,7 +118,9 @@ function observeNotifications(){
     const unreadChatCount = document.querySelector("#chat_dropdown > i");
     if(unreadNotificationCount && unreadChatCount){
         simulateOpenClose(unreadNotificationCount.parentElement);
+        NOTIF_LIST = unreadNotificationCount.parentElement.querySelector(".notification-list");
         simulateOpenClose(unreadChatCount.parentElement);
+        CHAT_LIST = unreadNotificationCount.parentElement.querySelector(".chat-list");
         OBSERVER.observe(unreadNotificationCount, UNREAD_CONFIG);
         OBSERVER.observe(unreadChatCount, UNREAD_CONFIG);
     } else {
